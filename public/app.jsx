d8ac26316d2f02f28e6ef782ff3db6b4a4b0452f
pank004 <91553537+pank004@users.noreply.github.com>
2021-10-22 17:35:06 +0530

    Initial commit

---
+++ public/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "../public/heading";
+import List from "../public/list";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
